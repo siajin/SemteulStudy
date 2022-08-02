9abefe5007d39801e9023335f72ae48553b348b4
sia_02 <53578618+siajin@users.noreply.github.com>
2022-08-02 22:30:40 +0900

    8월 1주 평일 과제 node

---
+++ 8월 1주 평일 과제 node/index.js
@@ -0,0 +1,36 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    console.log(url);
+    if (url === "/") {
+        res.write('<html>');
+        res.write('main');
+        res.write('<a href="/link1">link1</a>');
+        res.write('<a href="/link2">link2</a>');
+        res.write('</html>');
+        return res.end();
+    }
+    else if (url === "/link1") {
+        res.write('<html>');
+        res.write('link1');
+        res.write('<a href="/">back</a>');
+        res.write('</html>');
+        return res.end();
+    }
+    else if (url === "/link2") {
+        res.write('<html>');
+        res.write('link2');
+        res.write('<a href="/">back</a>');
+        res.write('</html>');
+        return res.end();
+    }
+    else {
+        res.write('<html>');
+        res.write('Wrong approach');
+        res.write('<a href="/">back</a>');
+        res.write('</html>');
+    }
+});
+
+server.listen(3000);
