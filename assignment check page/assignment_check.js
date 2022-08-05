c65300a404e1954af33550a3e359e1f05a1eb47f
sueelly <sueelly@naver.com>
2022-08-05 21:54:52 +0900

    오류수정중...흑

---
+++ assignment check page/assignment_check.js
@@ -0,0 +1,39 @@
+
+
+
+/*
+const list_upload = document.getElementById("list_upload");
+const list = document.getElementById("list");
+const calendar = document.getElementById("calendar");
+const code_view = document.getElementById("code_view");
+
+function viewListUpload(){
+  ReactDom.render(<ListUpload />, list_upload);
+}
+
+function ListUpload (){
+  const [date, setDate] = React.useState("");
+  const [assignment, setAssignment] =
+  return{
+    <input type=""
+    <input type=""
+    <input type="file">
+    <button onClick="Save">Save</button>
+  };
+}
+z
+function Save(){
+  ReactDom.render(<AssignmentDate />, list);
+}
+
+function AssignmentDate(){
+  return (
+    <div onClick={UploadToList}>{date}</div>
+  );
+}
+
+function UploadToList(){
+   ReactDom.render(<ListUpload />, list_upload);
+}
+function
+*/
